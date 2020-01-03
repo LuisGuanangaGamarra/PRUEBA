d02f0fe96099db37a894181ba188e48e203c2b5d
Luis Guananga <luisgamarra_97@hotmail.com>
2020-01-03 10:47:03 -0500

    Promesas, encadenamiento de promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,9 +1,27 @@
 const someThingWillHappen = () => {
     return new Promise((resolve, reject) => {
         if (true) {
-            resolve('OK');
+            resolve('Hey');
         } else {
             reject('Error');
+        }
+    });
+};
+
+const someThingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('OK');
+            }, 2000);
+
+        } else {
+            /*
+                Con el new Error al falla la promesa se muestra
+                el trace del codigo y la parte donde fallo
+            */
+            const error = new Error('Error Whoops');
+            reject(error);
         }
     });
 };
@@ -11,3 +29,15 @@ const someThingWillHappen = () => {
 someThingWillHappen()
     .then(response => console.log(response))
     .catch(error => console.error(error));
+someThingWillHappen2()
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+
+/*
+    Encadenamiento de promesas
+*/
+Promise.all([someThingWillHappen(), someThingWillHappen2()])
+    .then(response => {
+        console.log(response);
+    })
+    .catch(erro => console.error(erro));
